8155b08394b2e2ffbe8bbc342ed384d0b4fc68dc
ploszukiwacz <ploszukiwacz@noreply.git.hacklub.app>
2025-02-16 21:54:58 +0100

    cache.

--- src/sw.js
+++ src/sw.js
@@ -2,7 +2,7 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "femboySocial";
+const CACHE_NAME = "femboySocial";
 
 // Add whichever assets you want to pre-cache here:
 const PRECACHE_ASSETS = [
