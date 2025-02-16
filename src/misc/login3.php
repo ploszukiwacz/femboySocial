4922e4ab4330a73d86c0caeedede20ec17ed8fbe
ploszukiwacz <ploszukiwacz@noreply.git.hacklub.app>
2025-02-16 20:05:47 +0100

    pwa test nr 1

--- src/misc/login3.php
+++ src/misc/login3.php
@@ -9,16 +9,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Set the cookie
     setcookie("token", $token, time() + 86400 * 30, "/"); // Cookie expires in 30 days
+    header("Location: /");
 } ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>Login via a token - femboySocial</title>
+
+    <!-- favicons -->
+    <link rel="icon" type="image/png" href="/assets/favicons/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/assets/favicons/favicon.svg" />
+    <link rel="shortcut icon" href="/assets/favicons/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicons/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="FS" />
+    <link rel="manifest" href="/assets/favicons/site.webmanifest" />
+
     <style>
         body {
             font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
+            background-color: #1e1e2e;
             display: flex;
             justify-content: center;
             align-items: center;
@@ -26,10 +36,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             margin: 0;
         }
         h1 {
-            color: #333;
+            color: #cdd6f4;
         }
         form {
-            background: #ffffff;
+            background: #313244;
             padding: 20px;
             border-radius: 8px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
@@ -44,18 +54,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         label {
             margin-bottom: 5px;
             font-weight: bold;
+            color: #cdd6f4;
         }
         input[type="password"] {
             padding: 10px;
             border: 1px solid #ccc;
             border-radius: 4px;
             box-sizing: border-box;
+            background-color: #1e1e2e;
         }
         input[type="submit"] {
             margin-top: 15px;
             padding: 10px 20px;
             color: #fff;
-            background-color: #007bff;
+            background-color: #89b4fa;
             border: none;
             border-radius: 4px;
             cursor: pointer;
